42a6e54c158aacf9935ed437b99ca52ce5f10cb0
joaopaulo33 <joaopaulo33573@gmail.com>
2024-02-03 18:36:18 -0300

    Adição flatlist com cards

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -5,12 +8,18 @@ import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
