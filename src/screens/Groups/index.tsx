206f16d5bfb0787f4c0612532d73a2252cba7001
joaopaulo33 <joaopaulo33573@gmail.com>
2024-02-04 12:00:53 -0300

    Adiciona item vazio na flatlist de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,9 +6,10 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
 import { Container } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -19,6 +20,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
