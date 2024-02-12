7a5658f8bb2e5275c01b3b8ce4725acefa8fed12
joaopaulo33 <joaopaulo33573@gmail.com>
2024-02-12 19:41:29 -0300

    Interface de players

--- App.tsx
+++ App.tsx
@@ -8,20 +8,23 @@ import {
 
 import theme from "./src/theme";
 
-import { Groups } from "@screens/Groups";
 import { Loading } from "@components/Loading";
+
+import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Players /> : <Loading />}
+      </>
     </ThemeProvider>
   );
 }
