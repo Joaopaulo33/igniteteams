a4e0b112834cedbceb05852304bbef6464b1f6d4
joaopaulo33 <joaopaulo33573@gmail.com>
2024-03-05 21:43:03 -0300

    Listando players

--- src/storage/player/playerAddByGroup.ts
+++ src/storage/player/playerAddByGroup.ts
@@ -1,7 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AppError } from "@utils/AppError";
 
-import { PlayerStorageDTO } from "./playerStorageDTO";
+import { playersGetByGroup } from "./playersGetByGroup";
+import { PlayerStorageDTO } from "./PlayerStorageDTO";
 import { PLAYER_COLLECTION } from "@storage/group/storageConfig";
 
 export async function playerAddByGroup(
@@ -9,7 +10,19 @@ export async function playerAddByGroup(
   group: string
 ) {
   try {
-    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, "");
+    const storedPlayers = await playersGetByGroup(group);
+
+    const playerAlreadyExists = storedPlayers.filter(
+      (player) => player.name === newPlayer.name
+    );
+
+    if (playerAlreadyExists.length > 0) {
+      throw new AppError("Essa pessoa já está adicionada em um time aqui.");
+    }
+
+    const storage = JSON.stringify([...storedPlayers, newPlayer]);
+
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage);
   } catch (error) {
     throw error;
   }
